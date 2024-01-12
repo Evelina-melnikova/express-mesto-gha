6238ecf73d65693199961ba922b76ee3a4c30554
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-01-12 18:59:54 +0300

    start

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
-const { HTTP_STATUS_NOT_FOUND } = require('http2').constants;
-const express = require('express');
 const { json } = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
-const { router } = require('./routes/root');
+const router = require('./routes/root');
+const HttpCodes = require('./utils/constants');
 
 const { PORT = '3000', MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
 const app = express();
 
-app.use(json());
+mongoose.connect(MONGO_URL);
 
+app.use(json());
 app.use((req, res, next) => {
   req.user = {
     _id: '659d1522e9eff3d6521a56ce',
   };
-
   next();
 });
 
-app.use('/', router);
+app.use(router);
 
 app.use((req, res, next) => {
-  res.status(HTTP_STATUS_NOT_FOUND).send({ message: 'Страница не найдена' });
+  res.status(HttpCodes.notFoundErr).send({ message: 'Такого маршрута не существует' });
 
   next();
 });
 
-mongoose.connect(MONGO_URL);
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
