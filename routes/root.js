905b68941b48393a2f9f0ec368846662451548ac
Evelina Melnikova <melnikovaa45@yandex.ru>
2024-01-22 11:39:13 +0300

    почти готовый вариант

--- routes/root.js
+++ routes/root.js
@@ -5,9 +5,9 @@ const auth = require('../middlewares/authorization');
 const cardRouter = require('./cards');
 
 const router = Router();
+router.use('/', adminsRouter);
 router.use(auth);
 router.use('/', userRouter);
-router.use('/', adminsRouter);
 router.use('/', cardRouter);
 
 module.exports = router;
